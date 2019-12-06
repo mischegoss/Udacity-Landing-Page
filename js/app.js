82f486e1e74f56888e74468c85ab3adff2cab714
Tamar Auber <tamarauber@Tamars-MacBook-Pro.local>
2019-12-06 02:16:01 -0500

    Create navbar html dynamically

--- js/app.js
+++ js/app.js
@@ -6,7 +6,9 @@ let itemValue;
 let itemText;
 let listItem;
 let listValue;
-let listText;
+let newLine;
+let lineText;
+let unorderedList = document.getElementById("unordered-list");
 
 let containerCount =  document.getElementsByClassName("landing__container").length;
 
@@ -15,8 +17,13 @@ for (i = 1; i < (containerCount + 1); i++) {
     item = "#section" + i+ "-title"
     itemValue = document.querySelector(item);
     itemText = itemValue.textContent;
-    listItem = "#sample-nav-" + i;
-    listValue = document.querySelector(listItem);
-    listValue.innerText = itemText;   
-  }
+    newLine = document.createElement("li");
+    lineText = document.createTextNode(itemText);
+    listItem = "sample-nav-" + i;
+    newLine.setAttribute("id", listItem);
+    newLine.setAttribute("class", "nav-list-item");
+    newLine.appendChild(lineText);
+    unorderedList.appendChild(newLine)
+
+}
 
