024315bade2355dfe531cf5019e0a64ffb068b28
Tamar Auber <tamarauber@Tamars-MacBook-Pro.local>
2020-01-11 23:49:05 -0500

    First Draft

--- js/app.js
+++ js/app.js
@@ -1,78 +1,70 @@
-
 //Build the menu dynamically
 
-
 let unorderedList = document.getElementById("unordered-list");
-let containerCount =  document.getElementsByClassName("landing__container").length;
+let containerCount = document.getElementsByClassName("landing__container")
+  .length;
 
+for (i = 1; i < containerCount + 1; i++) {
+  let item = "#section" + i + "-title";
+  let itemValue = document.querySelector(item);
+  let itemText = itemValue.textContent;
+  let newLine = document.createElement("li");
+  let lineText = document.createTextNode(itemText);
+  let listItem = "sample-nav-" + i;
+  newLine.setAttribute("id", listItem);
+  newLine.setAttribute("class", "nav-list-item");
+  newLine.appendChild(lineText);
+  unorderedList.appendChild(newLine);
 
-for (i = 1; i < (containerCount + 1); i++) {
-    let item = "#section" + i+ "-title";
-    let itemValue = document.querySelector(item);
-    let itemText = itemValue.textContent;
-    let newLine = document.createElement("li");
-    let lineText = document.createTextNode(itemText);
-    let listItem = "sample-nav-" + i;
-    newLine.setAttribute("id", listItem);
-    newLine.setAttribute("class", "nav-list-item");
-    newLine.appendChild(lineText);
-    unorderedList.appendChild(newLine);
+  let itemTarget = document.getElementById("section" + i);
+  let listTarget = document.getElementById(listItem);
+  let buttonName = "section" + i + "-button";
+  let buttonToAdd = document.getElementById(buttonName);
 
-    
-    let itemTarget = document.getElementById("section" + i);
-    let listTarget = document.getElementById(listItem);
-    let buttonName = "section" + i + "-button";
-    let buttonToAdd = document.getElementById(buttonName)
+  listTarget.addEventListener("click", function() {
+    itemTarget.scrollIntoView();
 
-    listTarget.addEventListener("scroll", function() {
-        itemTarget.setAttribute("style", "border: 10px dotted green")
-    })
-
-    listTarget.addEventListener("click", function(){ 
-        itemTarget.scrollIntoView();
-        itemTarget.setAttribute("class", "your-active-class");
-        
-        buttonToAdd.innerHTML = "<button class='section-button' onclick='goToTop()'>Return to Top</button>"
-        
-
-        
+    buttonToAdd.innerHTML =
+      "<button class='section-button' onclick='goToTop()'>Return to Top</button>";
+  });
 }
-    )}
 
 function goToTop() {
-    document.documentElement.scrollTop = 0;
-    console.log(containerCount)
-    for (i = 1; i < (containerCount + 1); i++) {
-        let elementToCheck = document.getElementById("section"+i);
-        let buttonToDelete = document.getElementById("section" + i + "-button");
-        buttonToDelete.innerHTML = "";
+  document.documentElement.scrollTop = 0;
 
-        if (elementToCheck.classList.contains("your-active-class")) {
-        elementToCheck.classList.remove("your-active-class");
-    } 
-
-       
-
-        
-        }
-    }
-
-    
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {this.addStickyOnOffset()};
-
-// Get the header
-var header = document.getElementById("header");
-console.log(header)
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function addStickyOnOffset() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+  for (i = 1; i < containerCount + 1; i++) {
+    let elementToCheck = document.getElementById("section" + i);
+    let buttonToDelete = document.getElementById("section" + i + "-button");
+    buttonToDelete.innerHTML = "";
   }
 }
+
+function checkIfSectionInView() {
+  let isInViewport = function(elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+      bounding.top <= 50 &&
+      bounding.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  for (i = 1; i < containerCount + 1; i++) {
+    let sectionInFullView = document.getElementById("section" + i);
+
+    window.addEventListener(
+      "scroll",
+      function(event) {
+        if (isInViewport(sectionInFullView)) {
+          sectionInFullView.classList.add("your-active-class");
+        } else {
+          sectionInFullView.classList.remove("your-active-class");
+        }
+      },
+      false
+    );
+  }
+}
+checkIfSectionInView();
